aba1237f712bdee62a8e70d722e154dcd0cdd0a1
P-ian-O <70882357+P-ian-O@users.noreply.github.com>
2020-10-21 08:36:29 +0800

    Add files via upload

---
+++ Kaboom dragon/index.js
@@ -0,0 +1,23 @@
+class Kaboom_dragon {
+    constructor(){
+        this.name= "Kaboom_dragon";
+        this.weapon= "bomb";
+        this.enemy= "EVERYONE";
+        this.food= "EVERYONE";
+    }
+}
+
+Speech(){
+    if(damaged) {
+        say(Kaboom);
+    } else {
+        say(Dont_come_any_closer );
+    }
+    }
+}
+
+Kaboom(){
+    if(damaged) {
+        Kaboom()
+    }
+}
